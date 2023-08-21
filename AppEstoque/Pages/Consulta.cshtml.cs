f4b8ee0749b3633c48361f70ab3288e6b55a7f25
Kombait98 <nillson946@gmail.com>
2023-08-20 21:37:23 -0300

    Adicionar arquivos de projeto.

---
+++ AppEstoque/Pages/Consulta.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppEstoque.Pages
+{
+    public class ConsultaModel : PageModel
+    {
+        private readonly ILogger<ConsultaModel> _logger;
+
+        public ConsultaModel(ILogger<ConsultaModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
